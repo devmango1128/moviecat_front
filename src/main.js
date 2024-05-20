4bd55767869b25cd7e1a039c13a83c48be8a495c
devmango1128 <devmango1128@gmail.com>
2024-05-21 02:53:34 +0900

    join design

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import routes from './routes/index';
+import common from '@/assets/js/common.js'
 
 const app = createApp(App);
 
-app.use(routes);
+app.use(routes).use(common);
 app.mount('#app');
